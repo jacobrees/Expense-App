48518b24868f248280de5f9edfbc5b1aae0fd78b
jacobrees <jacob.rees97@yahoo.com>
2023-01-15 20:37:43 +0000

    Add unique keys to lists

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import NewExpense from './components/NewExpense/NewExpense'
 import { useState } from 'react'
 
 const DUMMY_EXPENSES = [
-  { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-  { title: 'Toilet Paper', amount: 94.67, date: new Date(2021, 5, 28) },
-  { title: 'New TV', amount: 294.67, date: new Date(2021, 7, 28) },
-  { title: 'New Desk (Wooden)', amount: 294.67, date: new Date(2021, 8, 28) },
+  { id: 'e1',title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+  { id: 'e2',title: 'Toilet Paper', amount: 94.67, date: new Date(2021, 5, 28) },
+  { id: 'e3',title: 'New TV', amount: 294.67, date: new Date(2021, 7, 28) },
+  { id: 'e4',title: 'New Desk (Wooden)', amount: 294.67, date: new Date(2021, 8, 28) },
 ];
 
 const App = () => {
