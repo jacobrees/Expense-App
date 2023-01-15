223fe923dc5756fd275bce5cda6a6171aa7d5a52
jacobrees <jacob.rees97@yahoo.com>
2023-01-15 20:30:15 +0000

    Render lists of data dynamically, add functioanlity to add to list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import ExpenseDisplay from './components/Expenses/ExpenseDisplay';
 import NewExpense from './components/NewExpense/NewExpense'
+import { useState } from 'react'
+
+const DUMMY_EXPENSES = [
+  { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+  { title: 'Toilet Paper', amount: 94.67, date: new Date(2021, 5, 28) },
+  { title: 'New TV', amount: 294.67, date: new Date(2021, 7, 28) },
+  { title: 'New Desk (Wooden)', amount: 294.67, date: new Date(2021, 8, 28) },
+];
 
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+  
   const addExpenseHandler = (expense) => {
-    console.log("in app.js")
-    console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
   };
-
-  const expenses = [
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'Toilet Paper', amount: 94.67, date: new Date(2021, 5, 28) },
-    { title: 'New TV', amount: 294.67, date: new Date(2021, 7, 28) },
-    { title: 'New Desk (Wooden)', amount: 294.67, date: new Date(2021, 8, 28) },
-  ];
 
   return (
     <div>
